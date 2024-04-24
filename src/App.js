0416d444419abaaa0a68c4dcafeb36b7253e0fc7
Raza Husain <razahussain3301@gmail.com>
2024-04-25 02:47:47 +0530

    project init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container center h-full w-full">
+   <div className="p-6 h-20 w-20 rounded-full bg-gray-500 flex justify-center items-center">
+    Hello there
+   </div>
+    </div>
+  );
+}
+
+export default App;
